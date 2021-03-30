a7da9d3be414d923a5b00ce4350eef345e86a4ac
Benoît Rognier <benoit.rognier@edulera.com>
2021-03-30 12:04:34 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { DAppProvider /*, useConnect, useAccountPkh, useReady*/ } from './dappstate.js';
 import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
 import { appName } from './settings';
 
 function App() {
@@ -22,18 +24,23 @@ function App() {
       <ThemeProvider theme={theme}>
       <div className="App">
         <header className="App-header">
-          <Typography variant="h1" style={{ fontFamily : 'Alegreya Sans SC, sans-serif' }}>Completium</Typography>
-          <Typography variant="h5">
-            Edit <code>src/App.js</code> and save to reload.
-          </Typography>
-          <a
-            className="App-link"
-            href="https://completium.com/dapps"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn everything about DApps
-          </a>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Typography variant="h1" style={{ fontFamily : 'Alegreya Sans SC, sans-serif' }}>Completium</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Typography variant="h5">
+                Edit <code>src/App.js</code> and save to reload.
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Link href="https://completium.com/dapps" target="_blank" rel="noopener noreferrer">
+                <Typography variant="h5">
+                  Learn everything about DApps
+                </Typography>
+              </Link>
+            </Grid>
+          </Grid>
         </header>
       </div>
       </ThemeProvider>
