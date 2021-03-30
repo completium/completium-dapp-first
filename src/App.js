1f3f065ed2e70507e169b6e9d7f863cdb0767d49
Benoît Rognier <benoit.rognier@edulera.com>
2021-03-30 12:27:51 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import { appName } from './settings';
+import WalletButton from './components/WalletButton';
 
 function App() {
   const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
@@ -23,18 +24,27 @@ function App() {
     <DAppProvider appName={appName}>
       <ThemeProvider theme={theme}>
       <div className="App">
-        <header className="App-header">
+        <header className="App-header" style={{ backgroundColor: theme.palette.background.default }}>
           <Grid container spacing={3}>
             <Grid item xs={12}>
-              <Typography variant="h1" style={{ fontFamily : 'Alegreya Sans SC, sans-serif' }}>Completium</Typography>
+              <Typography
+                variant="h1"
+                style={{ color: theme.palette.text.primary, fontFamily : 'Alegreya Sans SC, sans-serif' }}
+              >
+                Completium
+              </Typography>
             </Grid>
             <Grid item xs={12}>
-              <Typography variant="h5">
+              <Typography variant="h5" style={{ color: theme.palette.text.primary }}>
                 Edit <code>src/App.js</code> and save to reload.
               </Typography>
             </Grid>
             <Grid item xs={12}>
-              <Link href="https://completium.com/dapps" target="_blank" rel="noopener noreferrer">
+              <Link
+                href="https://completium.com/dapps"
+                target="_blank" rel="noopener noreferrer"
+                style={{ color: theme.palette.primary.light }}
+              >
                 <Typography variant="h5">
                   Learn everything about DApps
                 </Typography>
