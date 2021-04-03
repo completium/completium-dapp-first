74c7110b591974a29ebe3fe4ea20bcd9e76eedb0
Benoit Rognier <benoit.rognier@edukera.com>
2021-04-03 10:59:19 +0200

    adds font def

--- src/settings.js
+++ src/settings.js
@@ -4,3 +4,7 @@ export const network  = "edo2net"
 export const endpoint = "https://edonet.smartpy.io"
 
 export const contractAddress = "";
+
+// fonts
+export const courier = "Courier Prime, monospace";
+export const alegreya = "Alegreya Sans SC, sans-serif";
